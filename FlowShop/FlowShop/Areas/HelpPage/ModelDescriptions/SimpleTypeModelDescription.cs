3066cb62f2ee36ede29b1061b0142a1bc5ec1738
Fabricio Durand <fabricio.silva@viaflow.com.br>
2019-08-05 14:43:12 -0300

    Deletar projeto errado

--- FlowShop/FlowShop/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlowShop.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- FlowShop/FlowShop/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlowShop.WebApiApplication" Language="C#" %>
