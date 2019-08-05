febeee3ba1dde20932944ca37df1b4107a729afc
Luiz Mazoni <luiz.mazoni@viaflow.com.br>
2019-08-05 14:19:22 -0300

    gitignore

---
+++ POPULACAO.sql
@@ -0,0 +1,21 @@
+INSERT INTO PERFIL(NOME)
+VALUES 
+('Administrador'),
+('Usuario'),
+('Analista de Compras')
+
+INSERT INTO CATEGORIA (NOME)
+VALUES
+('Hardware'),
+('Software'),
+('Qualquer coisa')
+
+INSERT INTO _STATUS (NOME)
+VALUES
+('Aguardando aprovação'),
+('Aprovada'), --APROVADA = 1
+('Reprovada'), -- APROVADA = 0
+('Finalizada') -- FINALIZADO = 1
+
+
+
