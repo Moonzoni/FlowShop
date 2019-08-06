a872d40154a10acf98daad4f990033998752b25d
Luiz Mazoni <luiz.mazoni@viaflow.com.br>
2019-08-06 17:53:10 -0300

    aaa

---
+++ FlowShop_INFRA/Interface/IOrcamentoRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlowShop_INFRA.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowShop_INFRA.Interface
+{
+    public interface IOrcamentoRepository : IBaseRepository<OrcamentoEntity>
+    {
+
+    }
+}
